c1f3f6a518777f1f1775fac0862f75da5ddecea4
Alexander Fenster <fenster@google.com>
2020-03-25 01:47:26 -0700

    chore: fix compileProtos call

--- synth.py
+++ synth.py
@@ -44,4 +44,4 @@ s.copy(templates)
 # Node.js specific cleanup
 subprocess.run(["npm", "install"])
 subprocess.run(["npm", "run", "fix"])
-subprocess.run(["npx", "compileProtos", "run"])
+subprocess.run(["npx", "compileProtos", "src"])
